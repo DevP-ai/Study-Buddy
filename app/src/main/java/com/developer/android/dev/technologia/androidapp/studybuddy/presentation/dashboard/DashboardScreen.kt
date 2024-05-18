e6cf30c0316c8b3ba8f8a2fb824f596374665607
Devajit <patar1447@gmail.com>
2024-05-18 16:21:27 +0530

    Study Start Session Button Added

--- app/src/main/java/com/developer/android/dev/technologia/androidapp/studybuddy/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/studybuddy/presentation/dashboard/DashboardScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -75,6 +76,15 @@ fun DashboardScreen() {
                     onAddIconClicked = {},
                     onSubjectCardClick = {}
                 )
+            }
+
+            item{
+                Button(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(horizontal = 48.dp, vertical = 20.dp),
+                    onClick = { /*TODO*/ }) {
+                    Text(text = "Start Study Session")
+                }
             }
         }
     }
