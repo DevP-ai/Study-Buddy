d8bf09471c197374d2ffc2b3e4188eed1615bc56
Devajit <patar1447@gmail.com>
2024-05-24 15:59:02 +0530

    SessionScreen implemented with subject bottomsheet and session delete dialog

--- app/src/main/java/com/developer/android/dev/technologia/androidapp/studybuddy/presentation/session/SessionScreen.kt
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/studybuddy/presentation/session/SessionScreen.kt
@@ -1,10 +1,21 @@
 package com.developer.android.dev.technologia.androidapp.studybuddy.presentation.session
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,22 +23,151 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.developer.android.dev.technologia.androidapp.studybuddy.R
+import com.developer.android.dev.technologia.androidapp.studybuddy.domain.model.Session
+import com.developer.android.dev.technologia.androidapp.studybuddy.domain.model.Subject
+import com.developer.android.dev.technologia.androidapp.studybuddy.presentation.components.DeleteDialog
+import com.developer.android.dev.technologia.androidapp.studybuddy.presentation.components.SubjectListBottomSheet
+import com.developer.android.dev.technologia.androidapp.studybuddy.presentation.components.studySessionList
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SessionScreen() {
+
+    val subjects = listOf(
+        Subject(
+            subjectId = 0,
+            name = "English",
+            goalHours = 10f,
+            colors = Subject.subjectColors[0]
+        ),
+        Subject(
+            subjectId = 0,
+            name = "Hindi",
+            goalHours = 10f,
+            colors = Subject.subjectColors[1]
+        ),
+        Subject(
+            subjectId = 0,
+            name = "Science",
+            goalHours = 10f,
+            colors = Subject.subjectColors[2]
+        ),
+        Subject(
+            subjectId = 0,
+            name = "English",
+            goalHours = 10f,
+            colors = Subject.subjectColors[3]
+        ),
+        Subject(subjectId = 0, name = "English", goalHours = 10f, colors = Subject.subjectColors[4])
+    )
+
+    val scope = rememberCoroutineScope()
+    var isSubjectBottomSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val bottomSheetState = rememberModalBottomSheetState()
+    SubjectListBottomSheet(
+        sheetState = bottomSheetState,
+        isOpen = isSubjectBottomSheetOpen,
+        subjects = subjects,
+        onSubjectClick = {
+            scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
+                if (!bottomSheetState.isVisible) isSubjectBottomSheetOpen = false
+            }
+        },
+        onDismissRequest = { isSubjectBottomSheetOpen = false }
+    )
+
+    val sessions = listOf(
+        Session(0,"Physics",0L,0L,1),
+        Session(0,"English",0L,0L,1),
+        Session(0,"Hindi",0L,0L,1),
+        Session(0,"Physics",0L,0L,1),
+        Session(0,"Physics",0L,0L,1),
+        Session(0,"Physics",0L,0L,1),
+        Session(0,"Physics",0L,0L,1),
+        Session(0,"Physics",0L,0L,1)
+    )
+
+    var isDeleteDialogOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+    DeleteDialog(
+        title = "Delete Session?",
+        isOpen = isDeleteDialogOpen,
+        bodyText ="Are you sure, you want to delete this session?\nThis action can not be undone." ,
+        onDismissRequest = {
+            isDeleteDialogOpen=false},
+        onConfirmClick = {
+            isDeleteDialogOpen=false
+        })
+
     Scaffold(
-        topBar = { SessionScreenTopBar(
-            onBackButtonClick = {}
-        )}
+        topBar = {
+            SessionScreenTopBar(
+                onBackButtonClick = {}
+            )
+        }
     ) { paddingValues ->
-        LazyColumn (
-            modifier= Modifier
+        LazyColumn(
+            modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-        ){
+        ) {
+            item {
+                TimerSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                )
+            }
 
+            item {
+                RelatedToSubjectSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    relatedToSubject = "English",
+                    selectedSubjectButtonClick = {
+                        isSubjectBottomSheetOpen = true
+                    }
+                )
+            }
+
+            item {
+                ButtonsSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    startButtonClick = { },
+                    cancelButtonClick = {},
+                    finishedButtonClick = {}
+                )
+            }
+
+            studySessionList(
+                sectionTitle = "STUDY SESSIONS HISTORY",
+                sessions = sessions,
+                emptyListText = "You don't have any recent study sessions.\nStart a study session to begin recording your progress.",
+                onDeleteClick = {
+                    isDeleteDialogOpen = true
+                }
+            )
         }
     }
 }
@@ -53,4 +193,90 @@ fun SessionScreenTopBar(
             )
         }
     )
+}
+
+@Composable
+private fun TimerSection(
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(250.dp)
+                .border(5.dp, MaterialTheme.colorScheme.surfaceVariant, CircleShape)
+        )
+        Text(
+            text = "00:05:00",
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp)
+        )
+    }
+}
+
+@Composable
+private fun RelatedToSubjectSection(
+    modifier: Modifier,
+    relatedToSubject: String,
+    selectedSubjectButtonClick: () -> Unit
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Related to subject",
+            style = MaterialTheme.typography.bodySmall
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = relatedToSubject,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            IconButton(onClick = selectedSubjectButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Select subject"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonsSection(
+    modifier: Modifier,
+    startButtonClick: () -> Unit,
+    cancelButtonClick: () -> Unit,
+    finishedButtonClick: () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(onClick = cancelButtonClick) {
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp),
+                text = "Cancel"
+            )
+        }
+        Button(onClick = startButtonClick) {
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp),
+                text = "Start"
+            )
+        }
+        Button(onClick = finishedButtonClick) {
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp),
+                text = "Finish"
+            )
+        }
+    }
 }
