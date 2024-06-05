3f49f79d4f9a1568817c3372a071c9653d7a986c
Devajit <patar1447@gmail.com>
2024-06-05 14:15:46 +0530

    Room Database and Color Type Converter implemented

---
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/studybuddy/data/local/AppDatabase.kt
@@ -0,0 +1,23 @@
+package com.developer.android.dev.technologia.androidapp.studybuddy.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.developer.android.dev.technologia.androidapp.studybuddy.domain.model.Session
+import com.developer.android.dev.technologia.androidapp.studybuddy.domain.model.Subject
+import com.developer.android.dev.technologia.androidapp.studybuddy.domain.model.Task
+
+@Database(
+    entities = [Subject::class,Session::class, Task::class],
+    version = 1
+)
+@TypeConverters(ColorListConverter::class)
+abstract class AppDatabase:RoomDatabase(){
+
+    abstract fun subjectDao():SubjectDao
+
+    abstract  fun sessionDao():SessionDao
+
+    abstract fun taskDao():TaskDao
+
+}
