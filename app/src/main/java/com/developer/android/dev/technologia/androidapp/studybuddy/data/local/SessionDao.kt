af4f429ae2f9734cb3130f9c083fafebe269f348
Devajit <patar1447@gmail.com>
2024-06-05 13:37:35 +0530

    SessionDao implemented

---
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/studybuddy/data/local/SessionDao.kt
@@ -0,0 +1,38 @@
+package com.developer.android.dev.technologia.androidapp.studybuddy.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.developer.android.dev.technologia.androidapp.studybuddy.domain.model.Session
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface SessionDao {
+
+    @Insert
+    suspend fun insertSession(session: Session)
+
+    @Delete
+    suspend fun deleteSession(session: Session)
+
+    @Query("SELECT * FROM Session")
+    fun getAllSessions():Flow<List<Session>>
+
+    @Query("SELECT * FROM Session WHERE sessionSubjectId=:subjectId")
+    fun getRecentSessionsForSubject(subjectId:Int):Flow<List<Session>>
+
+    @Query("SELECT SUM(duration) FROM Session")
+    fun getTotalSessionDuration():Flow<Long>
+
+
+    @Query("SELECT SUM(duration) FROM Session WHERE sessionSubjectId=:subjectId")
+    fun getTotalSessionDurationBySubjectId(subjectId: Int):Flow<Long>
+
+
+    @Query("DELETE FROM Session WHERE sessionSubjectId=:subjectId")
+    fun deleteSessionBySubjectId(subjectId: Int)
+
+
+}
